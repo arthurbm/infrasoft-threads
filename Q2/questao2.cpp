f3777ee5bab6bb6d1f4aafbc5607d5263b044a1d
Arthur Brito Medeiros <arthurmedeiros32@gmail.com>
2021-12-13 00:55:52 -0300

    Hotfix(main): adicionar comentários na questao 2

--- Q2/questao2.cpp
+++ Q2/questao2.cpp
@@ -18,9 +18,9 @@ void *routine(void *arg)
   int stop = 0;
   for (int i = t_arg->start; (i < t_arg->fim) && !stop; i++)
   {
-    if (t_arg->list[i] > t_arg->list[i + 1])
+    if (t_arg->list[i] > t_arg->list[i + 1]) // verifica se o proximo é menor
     {
-      *(t_arg->is_ordered) = 0;
+      *(t_arg->is_ordered) = 0; // marca como desordenado
       stop = 1;
     }
   }
@@ -33,20 +33,20 @@ void *routine(void *arg)
 struct Thread
 {
   pthread_t thread;
-  int id;
-  int *is_ordered = new int();
-  ThreadArg *t_arg;
-  void initThread(ThreadArg *arg)
+  int id;                         // foi criado para debugar as threads
+  int *is_ordered = new int();    // alocando um ponteiro pra int
+  ThreadArg *t_arg;               // argumento da thread
+  void initThread(ThreadArg *arg) // executa a rotina
   {
     t_arg = arg;
     t_arg->is_ordered = is_ordered; // ponteiro para a flag
     pthread_create(&thread, NULL, routine, arg);
   }
-  void await()
+  void await() // espera a thread ser finalizada
   {
     pthread_join(thread, NULL);
   };
-  void print_list()
+  void print_list() // usado para debug
   {
     cout << "=-=-=-= id:" << id << " =-==-=\n";
     for (int i = t_arg->start; i <= t_arg->fim; i++)
@@ -59,8 +59,8 @@ struct Thread
 
 int main()
 {
-  int N = 0;
-  int v_size;
+  int N = 0;  // numero de threads
+  int v_size; // tamanho da lista
   int array_is_ordered = 1;
 
   cout << "Digite N:\n";
@@ -76,21 +76,21 @@ int main()
     cin >> vetor[i];
   }
 
-  Thread threads[N];
-  ThreadArg *thread_args[N];
+  Thread threads[N];         // crio as threads
+  ThreadArg *thread_args[N]; // crio os argumentos
 
   // inicializando as threads
   for (int i = 0; i < N; i++)
   {
     int step = v_size / N;
     ThreadArg *t_arg = new ThreadArg(); // criando os argumentos da rotina
-    Thread t; // criando a thread
+    Thread t;                           // criando a thread
     t.id = i;
     t_arg->list = vetor;
     t_arg->start = step * i;
     if (i == N - 1)
     {
-      t_arg->fim = v_size - 1;
+      t_arg->fim = v_size - 1; // recebe o resto da lista caso a divisão nao seja inteira
     }
     else
     {
@@ -120,7 +120,8 @@ int main()
     };
     // threads[i].print_list();
   };
-
+  // Se qualquer uma das partes do array que estão sendo checadas pelas threads estiverem desordenadas,
+  // então o array como um todo estará desordenado
   if (array_is_ordered)
   {
     cout << "O array está ordenado!!\n";
@@ -129,5 +130,7 @@ int main()
   {
     cout << "O array não está ordenado!!\n";
   }
+  // Só faltou dar delete nos ponteiros (Caso o professor pergunte)
+  // Na prática não precisamos passar o pthread_exit pois já damos o p_thread_join em todas as 
   return 1;
 }
